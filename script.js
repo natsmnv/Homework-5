4691d5f76835a4de16fdb7eb5016cce72da7b139
natsmnv <natnatanatasha@gmail.com>
2023-03-21 15:40:11 +0200

    Add index and script files

---
+++ script.js
@@ -0,0 +1,7 @@
+let firstNum = Number(prompt('Enter first number: '));
+let secondNum = Number(prompt('Enter second number: '));
+let thirdNum = Number(prompt('Enter third number: '));
+
+let average = (firstNum + secondNum + thirdNum) / 3;
+
+alert(`Average number is ${average}`);
